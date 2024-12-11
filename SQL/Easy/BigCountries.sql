7b39ea7818836054338e93396143e041279700df
Matthew Finster <mgfinster@gmail.com>
2024-12-11 18:18:11 +0100

    Updating BigCountries solution file

--- SQL/Easy/BigCountries.sql
+++ SQL/Easy/BigCountries.sql
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 -- Problem: Big Countries (LeetCode #595)
 -- Difficulty: Easy
 -- Link: https://leetcode.com/problems/big-countries/
 
 SELECT name, population, area
 FROM World
-=======
--- Problem: Big Countries (LeetCode #595)
--- Difficulty: Easy
--- Link: https://leetcode.com/problems/big-countries/
-
-SELECT name, population, area
-FROM World
->>>>>>> 6384377afe47b08faddacc2d2cb4d3c636c4dd27
 WHERE area >= 3000000 OR population >= 25000000
